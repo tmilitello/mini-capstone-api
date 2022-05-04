4c6cb476bee8408fe6f1d551130b776be33a09c5
Thomas J Militello <thomasjmilitello93@gmail.com>
2022-05-04 12:57:23 -0500

    adds product instance to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,4 @@ Product.create(name: "chocolates", price: 4, image_url: "https://www.history.com
 Product.create(name: "swell bottle", price: 10, image_url: "https://d34kame2p3gj5k.cloudfront.net/media/uploads/2018/06/21141931/17oz-Onyx-bottle-front-cap-on.png", description: "swell bottle, dark gray")
 
 
+Product.create(name: "test", price: 10, image_url: "test", description: "test")
