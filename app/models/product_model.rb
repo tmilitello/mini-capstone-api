98c861fe3858c158a6dcadc778a244bfee79c732
Thomas J Militello <thomasjmilitello93@gmail.com>
2022-05-04 11:32:01 -0500

    adds Product model

--- app/models/product_model.rb
+++
@@ -1,2 +0,0 @@
-class ProductModel < ApplicationRecord
-end
--- db/migrate/20220504163118_create_products.rb
+++ db/migrate/20220504163118_create_products.rb
@@ -1,6 +1,6 @@
-class CreateProductModels < ActiveRecord::Migration[7.0]
+class CreateProducts < ActiveRecord::Migration[7.0]
   def change
-    create_table :product_models do |t|
+    create_table :products do |t|
       t.string :name
       t.integer :price
       t.string :image_url
