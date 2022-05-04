46bb7129c9309364e53370075bb7e18bd7f38ded
Thomas J Militello <thomasjmilitello93@gmail.com>
2022-05-04 11:50:21 -0500

    adds route and method

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,6 @@
 class ProductsController < ApplicationController
+  def available_products_method
+    display_products = Product.all
+    render json: display_products.as_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "/available_products", controller: "products", action: "available_products_method"
 end
+
+#get "/hello_path", controller: "example_pages", action: "hello_method"
