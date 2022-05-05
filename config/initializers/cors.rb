d3783f52faee25b3f12202726a0ef88e048e4765
Thomas J Militello <thomasjmilitello93@gmail.com>
2022-05-05 12:01:33 -0500

    adds cors page

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -14,3 +14,12 @@
 #       methods: [:get, :post, :put, :patch, :delete, :options, :head]
 #   end
 # end
+
+# config/initializers/cors.rb
+
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins 'https://mini-capstone.peterxjang.com/','localhost:8080'
+    resource '*', headers: :any, methods: [:get, :post, :patch, :put]
+  end
+end
