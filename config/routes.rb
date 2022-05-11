7aa5893c7062dc4985cadf6eb037fb78f50010f3
Thomas J Militello <thomasjmilitello93@gmail.com>
2022-05-11 15:54:43 -0500

    adds a supplier controller

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,12 @@ Rails.application.routes.draw do
   patch "/products/:id" => "products#update"
   delete "/products/:id" => "products#destroy"
 
+  get "/suppliers" => "suppliers#index"
+  get "/suppliers/:id" => "suppliers#show"
+  post "/suppliers" => "suppliers#create"
+  patch "/suppliers/:id" => "suppliers#update"
+  delete "/suppliers/:id" => "suppliers#destroy"
+
+
 end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
-Product.create(name: "chocolates", price: 4, image_url: "https://www.history.com/.image/t_share/MTc3OTk5Njc1MTU5MjI1OTY1/valentines-day-chocolate-gettyimages-923430892.jpg", description: "box of chocolates")
+Product.create(name: "chocolates", price: 4, image_url: "https://www.history.com/.image/t_share/MTc3OTk5Njc1MTU5MjI1OTY1/valentines-day-chocolate-gettyimages-923430892.jpg", description: "box of chocolates", supplier_id: 1)
 
-Product.create(name: "swell bottle", price: 10, image_url: "https://d34kame2p3gj5k.cloudfront.net/media/uploads/2018/06/21141931/17oz-Onyx-bottle-front-cap-on.png", description: "swell bottle, dark gray")
+Product.create(name: "swell bottle", price: 10, image_url: "https://d34kame2p3gj5k.cloudfront.net/media/uploads/2018/06/21141931/17oz-Onyx-bottle-front-cap-on.png", description: "swell bottle, dark gray", supplier_id: 3)
 
 
-Product.create(name: "test", price: 10, image_url: "test", description: "test")
+Product.create(name: "test", price: 10, image_url: "test", description: "test", supplier_id: 3)
 
 Supplier.create(name: "Nike", email: "nike@gmail.com", phone_number: "314-622-8433")
 
