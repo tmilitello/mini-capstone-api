22438cb8821e65be5fc125cc634abeeedd8c77f1
Thomas J Militello <thomasjmilitello93@gmail.com>
2022-05-11 16:00:34 -0500

    adds suppliers model and http

--- app/controllers/suppliers_controller.rb
+++ app/controllers/suppliers_controller.rb
@@ -1,2 +1,8 @@
 class SuppliersController < ApplicationController
+
+  def index
+    @suppliers = Supplier.all
+    render json: {message: @suppliers}
+    # render template: "suppliers/index"
+  end
 end
