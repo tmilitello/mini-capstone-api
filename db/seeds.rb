c551e99e2327ca6274ec7ccd4240c55cdd9519d9
Thomas J Militello <thomasjmilitello93@gmail.com>
2022-05-11 15:46:32 -0500

    adds migration

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,3 +4,10 @@ Product.create(name: "swell bottle", price: 10, image_url: "https://d34kame2p3gj
 
 
 Product.create(name: "test", price: 10, image_url: "test", description: "test")
+
+Supplier.create(name: "Nike", email: "nike@gmail.com", phone_number: "314-622-8433")
+
+Supplier.create(name: "Addidas", email: "addidas@gmail.com", phone_number: "314-622-8432")
+
+Supplier.create(name: "Lululemon", email: "lululemon@gmail.com", phone_number: "314-622-5433")
+
