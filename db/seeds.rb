61b4900f2e2f0b42b5f09963b270af39a2743d53
Thomas J Militello <thomasjmilitello93@gmail.com>
2022-05-12 13:31:28 -0500

    adds images

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,7 @@ Product.create(name: "chocolates", price: 4, description: "box of chocolates", s
 
 Product.create(name: "swell bottle", price: 10, description: "swell bottle, dark gray", supplier_id: 3)
 
-
-Product.create(name: "test", price: 10, image_url: "test", description: "test", supplier_id: 3)
+Product.create(name: "test", price: 10, description: "test", supplier_id: 3)
 
 Supplier.create(name: "Nike", email: "nike@gmail.com", phone_number: "314-622-8433")
 
@@ -11,6 +10,6 @@ Supplier.create(name: "Addidas", email: "addidas@gmail.com", phone_number: "314-
 
 Supplier.create(name: "Lululemon", email: "lululemon@gmail.com", phone_number: "314-622-5433")
 
-Image.create(url: "https://www.thespruceeats.com/thmb/eEhUUpSnXQCOV_nPMFLktfSui3k=/940x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/chocolate_hero1-d62e5444a8734f8d8fe91f5631d51ca5.jpg")
+Image.create(url: "https://www.thespruceeats.com/thmb/eEhUUpSnXQCOV_nPMFLktfSui3k=/940x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/chocolate_hero1-d62e5444a8734f8d8fe91f5631d51ca5.jpg", product_id: 1)
 
-Image.create(url: "https://d34kame2p3gj5k.cloudfront.net/media/uploads/2021/11/10065943/20210521_SHOT_39_0009-3-1-2.png")
+Image.create(url: "https://d34kame2p3gj5k.cloudfront.net/media/uploads/2021/11/10065943/20210521_SHOT_39_0009-3-1-2.png", product_id: 2)
