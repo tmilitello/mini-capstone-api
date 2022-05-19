c0bf75c050aeaeb6a58f4b1b39b30438bc087ef5
Thomas J Militello <thomasjmilitello93@gmail.com>
2022-05-19 12:59:34 -0500

    adds http file for CP

--- app/controllers/carted_products_controller.rb
+++ app/controllers/carted_products_controller.rb
@@ -1,2 +1,13 @@
 class CartedProductsController < ApplicationController
+
+  def create
+    CartedProduct.create(
+      user_id: params[:user_id],
+      product_id: params[:product_id],
+      quantity: params[:quantity],
+      status: params[:status],
+      order_id: params[:order_id]
+    )
+  end
+
 end
