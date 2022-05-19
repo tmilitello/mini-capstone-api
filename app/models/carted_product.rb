60905bea52badbfc21fd44eb666d4fea4a854a75
Thomas J Militello <thomasjmilitello93@gmail.com>
2022-05-19 12:24:28 -0500

    adds associations of model

--- app/models/carted_product.rb
+++ app/models/carted_product.rb
@@ -1,2 +1,4 @@
 class CartedProduct < ApplicationRecord
+  belongs_to :order, optional: true
+  belongs_to :product
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -8,7 +8,8 @@ class Product < ApplicationRecord
   validates :description, presence: true
   has_many :images
   belongs_to :supplier
-  has_many :orders
+  has_many :carted_products
+  has_many :orders, through: :carted_products
   has_many :category_products
   has_many :categories, through: :category_products
 
